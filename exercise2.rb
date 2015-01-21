eb66d7ef64ddaded179d3d6b4509549636672962
Nicole Rego <nicole.rego@live.com>
2015-01-21 14:17:08 -0500

    Exercise two

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+#Calculating tip
+meal = 55
+tip = 0.15
+
+puts "A good tip for a #{meal} dollar meal is " + (meal*tip).to_s
+
+#Converting string to an integer
+puts "Hello World " + 8.to_s
+
+#Calculator/String interpolation
+puts "The result is " + (45628 * 7839).to_s
+
+#true
+puts (true && false) || (false && true) || !(false && false)
